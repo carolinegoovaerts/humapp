dea7abace26fd8b11094b276e88d9f82e07f79b4
Caroline Goovaerts <caroline@goovaerts.nl>
2017-12-05 17:57:41 +0100

    Identify reporting template on page

--- src/app/reporting/atomic/templates/reporting.template.ts
+++ src/app/reporting/atomic/templates/reporting.template.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-reporting',
-  templateUrl: './reporting.template.html'
+  templateUrl: './reporting.template.html',
+  styleUrls: ['./reporting.template.css']
 })
 export class ReportingComponent {
 }
