f6f9c6c4090936a8ba9e5ef930d99d08b46c051b
Caroline Goovaerts <caroline@goovaerts.nl>
2017-12-06 23:51:30 +0100

    Add TODO

--- src/app/reporting/atomic/molecules/map/map.molecule.ts
+++ src/app/reporting/atomic/molecules/map/map.molecule.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import * as L from 'leaflet';
 
+// TODO fix improper displaying of tiles
 @Component({
   selector: 'app-map',
   templateUrl: './map.molecule.html',
